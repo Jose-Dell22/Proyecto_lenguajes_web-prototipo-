297b8151c782d56c3b3e15b7a6d85ffd1b8f31d4
Miguel Cordoba Sanchez <miguecell33@gmail.com>
2025-10-13 17:50:39 -0500

    feat(about):Se añadio el componente AboutUs, se modifico navbar y rutas (App.jsx) y se añadio el cdn de Semantic ui en index.html

---
+++ src/components/AboutUs/About.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Aboutstyle.css"; 
+
+const AboutUs = () => {
+  return (
+    <div className="aboutus-container">
+      <h2 className="aboutus-title">Sobre Nosotros</h2>
+
+      <p className="aboutus-text">
+        
+      </p>
+
+      <p className="aboutus-text">
+        A través de este proyecto, buscamos entender cómo funcionan los componentes,
+        las rutas y la organización de un proyecto real en React. También queremos
+        desarrollar habilidades en diseño y estructura de código limpio.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
